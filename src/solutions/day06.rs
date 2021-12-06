4e8e1ac8ee5ce36d242c16fc1a1198812350ccb2
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2021-12-06 15:04:23 +0100

    optimize: day 06 * use a fixed sized array in favor of a map (10x faster)

--- src/solutions/day06.rs
+++ src/solutions/day06.rs
@@ -1,68 +1,61 @@
-use std::collections::HashMap;
+static REPRO_INTERVAL_INITIAL: usize = 9;
+static REPRO_INTERVAL: usize = 7;
 
-static REPRO_INTERVAL_INITIAL: u64 = 9;
-static REPRO_INTERVAL: u64 = 7;
-
-type Generations = HashMap<u64, u64>;
-
-fn get_initial_members(input: &str) -> Vec<usize> {
+fn get_og_fishes(input: &str) -> Vec<usize> {
     input.split(',').map(|s| s.parse().unwrap()).collect()
 }
 
-fn project_adult_generation(
-    generations: &mut Generations,
-    generation_size: u64,
-    current_interval: u64,
-    interval_count: u64,
-) {
-    let mut spawns_at = current_interval + REPRO_INTERVAL;
-    // increase the generation_size at {interval} by the current generation_size for rest of observed interval.
-    while spawns_at <= interval_count {
-        *generations.entry(spawns_at).or_default() += generation_size;
-        spawns_at += REPRO_INTERVAL;
-    }
-}
+fn project_population(members: Vec<usize>, generations: &mut [usize]) -> usize {
+    let interval_count = generations.len();
+    let mut pop = members.len();
 
-fn project_population(members: Vec<usize>, interval_count: u64) -> u64 {
-    let mut generations: Generations = HashMap::new();
-    let mut pop: u64 = members.len() as u64;
-
-    // for each fish in the OG generation, spawn an offspring at {rest_timer} and project the fish's adult life.
-    members.iter().for_each(|_i| {
-        let i = *_i as u64;
-        *generations.entry(i).or_default() += 1;
-        // once the OG fish generation spawns their first offspring, they continue reproducing.
-        project_adult_generation(&mut generations, 1, i, interval_count);
+    // for each fish in the OG generation, spawn an offspring at {rest_timer}.
+    // we have to project their adult life as well since OGs continue reproducing.
+    members.iter().for_each(|f| {
+        generations[*f] += 1;
+        project_adult_generation(generations, 1, *f, interval_count);
     });
 
-    // a generation of fishes hatches every day. since there is no variance in reproduction rate, we can project it as a whole.
+    // a generation of fishes hatches every day.
+    // we can project them as a whole since there is no variance in repro. rate in a generation.
     for i in 0..interval_count {
-        let generation_size = *generations.entry(i).or_default();
-        // add hatched fishes to the population.
+        let generation_size = generations[i];
+        // add hatched fishes to the population counter.
         pop += generation_size;
 
+        // initial reproduction is delayed for hatched fishes. we handle it with a special case.
         let adults_at = i + REPRO_INTERVAL_INITIAL;
 
-        // initial reproduction is slightly delayed for hatched fishes.
-        // we handle it here before projecting the rest of interval_count in a loop.
-        if adults_at <= interval_count {
-            *generations.entry(adults_at).or_default() += generation_size;
+        if adults_at < interval_count {
+            generations[adults_at] += generation_size;
             // Once fishes are adults, we can project the rest of {interval_count} in a loop.
-            project_adult_generation(&mut generations, generation_size, adults_at, interval_count);
+            project_adult_generation(generations, generation_size, adults_at, interval_count);
         }
     }
 
     pop
 }
 
-pub fn part_one(input: &str) -> u64 {
-    let members = get_initial_members(input);
-    project_population(members, 80)
+fn project_adult_generation(
+    generations: &mut [usize],
+    generation_size: usize,
+    current_interval: usize,
+    interval_count: usize,
+) {
+    let mut spawns_at = current_interval + REPRO_INTERVAL;
+    // increase the generation_size at {interval} by the current generation_size for rest of observed interval.
+    while spawns_at < interval_count {
+        generations[spawns_at] += generation_size;
+        spawns_at += REPRO_INTERVAL;
+    }
 }
 
-pub fn part_two(input: &str) -> u64 {
-    let members = get_initial_members(input);
-    project_population(members, 256)
+pub fn part_one(input: &str) -> usize {
+    project_population(get_og_fishes(input), &mut [0; 80])
+}
+
+pub fn part_two(input: &str) -> usize {
+    project_population(get_og_fishes(input), &mut  [0; 256])
 }
 
 #[test]
